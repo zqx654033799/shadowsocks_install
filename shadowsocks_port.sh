965813ccdadb2fe3ca2465d2475ac12d61fefa4b
zqx654033799 <zqx654033799@qq.com>
2017-05-22 09:38:16 +0800

    Update shadowsocks_port.sh

--- shadowsocks_port.sh
+++ shadowsocks_port.sh
@@ -2,13 +2,13 @@
 
 port=8381
 sleep_time=30
-max_bandwidth=1024000
+max_bandwidth=1024000000000
 basepath=`dirname $0`
 
 sleep 10
 
 file_bandwidth=0
-for i in `cat $basepath/shadowsocks_$port.log`;
+for i in `cat $basepath/shadowsocks_$port.db`;
 do
 let "file_bandwidth+=i"
 done
@@ -18,9 +18,12 @@ if [ "$file_bandwidth" -gt "$max_bandwidth" ]; then
 echo "exit"
 exit
 else
-dpt_string=`iptables -vxn -L | grep -i "dpt:$port"`
-if [ ! -n "$dpt_string" ]; then
+udp_dpt_string=`iptables -vxn -L | grep -i "udp dpt:$port"`
+tcp_dpt_string=`iptables -vxn -L | grep -i "tcp dpt:$port"`
+if [ ! -n "$udp_dpt_string" -o ! -n "$tcp_dpt_string" ]; then
+`iptables -I INPUT -p udp --dport $port -j ACCEPT`
 `iptables -I INPUT -p tcp --dport $port -j ACCEPT`
+`iptables -I OUTPUT -p udp --sport $port`
 `iptables -I OUTPUT -p tcp --sport $port`
 fi
 fi
@@ -28,19 +31,27 @@ fi
 while true
 do
 count_bandwidth=$file_bandwidth
-dpt_bandwidth=`iptables -vxn -L | grep -i "dpt:$port" | awk '{print $2}'`
-spt_bandwidth=`iptables -vxn -L | grep -i "spt:$port" | awk '{print $2}'`
-echo "dpt_bandwidth :$dpt_bandwidth"
-echo "spt_bandwidth :$spt_bandwidth"
-let "count_bandwidth+=dpt_bandwidth"
-let "count_bandwidth+=spt_bandwidth"
+udp_dpt_bandwidth=`iptables -vxn -L | grep -i "udp dpt:$port" | awk '{print $2}'`
+udp_spt_bandwidth=`iptables -vxn -L | grep -i "udp spt:$port" | awk '{print $2}'`
+tcp_dpt_bandwidth=`iptables -vxn -L | grep -i "tcp dpt:$port" | awk '{print $2}'`
+tcp_spt_bandwidth=`iptables -vxn -L | grep -i "tcp spt:$port" | awk '{print $2}'`
+echo "udp_dpt_bandwidth :$udp_dpt_bandwidth"
+echo "udp_spt_bandwidth :$udp_spt_bandwidth"
+echo "tcp_dpt_bandwidth :$tcp_dpt_bandwidth"
+echo "tcp_spt_bandwidth :$tcp_spt_bandwidth"
+let "count_bandwidth+=udp_dpt_bandwidth"
+let "count_bandwidth+=udp_spt_bandwidth"
+let "count_bandwidth+=tcp_dpt_bandwidth"
+let "count_bandwidth+=tcp_spt_bandwidth"
 echo "count bandwidth :$count_bandwidth"
 
-`echo "$count_bandwidth" > $basepath/shadowsocks_$port.log`
-echo "write file $basepath/shadowsocks_$port.log"
+`echo "$count_bandwidth" > $basepath/shadowsocks_$port.db`
+echo "write file $basepath/shadowsocks_$port.db"
 
 if [ "$count_bandwidth" -gt "$max_bandwidth" ]; then
+`iptables -D INPUT -p udp --dport $port -j ACCEPT`
 `iptables -D INPUT -p tcp --dport $port -j ACCEPT`
+`iptables -D OUTPUT -p udp --sport $port`
 `iptables -D OUTPUT -p tcp --sport $port`
 echo "port delete: $port"
 exit
